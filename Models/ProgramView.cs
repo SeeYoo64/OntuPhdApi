289dbc460b26a09426628bee23f9d9845e8b5345
bebraman <bebraman@HOME-PC>
2025-03-23 03:29:35 +0200

    Fixed ProgramView model

--- Models/ProgramView.cs
+++ Models/ProgramView.cs
@@ -3,23 +3,30 @@
     public class ProgramView
     {
         public int Id { get; set; }
-        public string Name { get; set; } // Две строки названия
-        public string FieldOfStudy { get; set; } // fieldCode + fieldName
-        public string Speciality { get; set; } // specialityCode + specialityName
-        public List<string> Form { get; set; } // Массив форм обучения
+        public string Name { get; set; }
+        public FieldOfStudy FieldOfStudy { get; set; }  // Объект с code и name
+        public Speciality Speciality { get; set; }      // Объект с code и name
+        public List<string> Form { get; set; }
         public int Years { get; set; }
         public int Credits { get; set; }
         public decimal Sum { get; set; }
-        public List<decimal> Costs { get; set; } // Массив стоимости по годам
+        public List<decimal> Costs { get; set; }
         public ProgramCharacteristics ProgramCharacteristics { get; set; }
         public ProgramCompetence ProgramCompetence { get; set; }
         public ProgramResults ProgramResults { get; set; }
         public string LinkFaculty { get; set; }
         public string LinkFile { get; set; }
-        public List<ProgramComponent> UniversalComponents { get; set; }
-        public List<ProgramComponent> PracticalComponents { get; set; }
-        public List<ProgramComponent> OptionalComponents { get; set; }
+    }
 
+    public class FieldOfStudy
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
 
+    public class Speciality
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
     }
 }
