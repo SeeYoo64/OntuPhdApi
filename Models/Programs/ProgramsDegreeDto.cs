ce9e30bfce2d83b6e19cc9ea9e98f7cc3eef8418
bebraman <bebraman@HOME-PC>
2025-04-03 00:35:40 +0300

    Fixed ProgramDegreeDto

--- Models/Programs/ProgramsDegreeDto.cs
+++ Models/Programs/ProgramsDegreeDto.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Degree { get; set; }
         public string Name { get; set; }
-        public string NameEng { get; set; }
         public FieldOfStudy FieldOfStudy { get; set; }
         public Speciality Speciality { get; set; }
     }
